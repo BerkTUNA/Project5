ee79cb7929c341be6286e59a3d3148ebe65ce2b8
Berk TUNA <berk.tuna@hotmail.com.tr>
2020-11-10 15:26:32 +0300

    Codes
    Version 1
    places to fix;
    make menu scene(Play,Credit,Exit)
    Score is written on the console but I couldn't print it on the screen
    if the down key is held down,fast put it down items

---
+++ Proje5/Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public TetrisBlock tetris;
+    public Text text;
+
+    void Start()
+
+    {
+        text = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       // text.text = "Score: " + tetris.scoretext;
+
+    }
+}
